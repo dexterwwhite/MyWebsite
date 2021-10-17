7307f2d7323b61d76251acb7174b8a0984fcfb69
Dexter White <dexter.w.white12@gmail.com>
2021-10-16 22:55:33 -0500

    Updated style all around

--- functions.js
+++ functions.js
@@ -1,9 +1,35 @@
-function changeBackgroundColor(color){
-	var element = document.body;
-	element.classList.toggle("dark-mode");
+function changeBackgroundColor()
+{
+	var colorNum = localStorage.getItem("bgcolor");
+	if(colorNum == 1)
+	{
+		localStorage.setItem("bgcolor", 0);
+		document.body.style.backgroundColor = "#113E3D";
+	}
+	else
+	{
+		localStorage.setItem("bgcolor", 1);
+		document.body.style.backgroundColor = "#4DC9C4";
+	}
 }
 
-window.onload = function(){
-	var color = localStorage.getItem("changedColor");
-	changeBackground(color);
+window.onload = function()
+{
+	var color = localStorage.getItem("bgcolor");
+	if(color === null)
+	{
+		localStorage.setItem("bgcolor", 1);
+		document.body.style.backgroundColor = "#4DC9C4";
+	}
+	else
+	{
+		if(color == 1)
+		{
+			document.body.style.backgroundColor = "#4DC9C4";
+		}
+		else
+		{
+			document.body.style.backgroundColor = "#113E3D";
+		}
+	}
 }
