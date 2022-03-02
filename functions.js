1da1f86910e6476f167735944f24f6958c4a74da
dexterwwhite <dexterwwhite@tamu.edu>
2022-03-02 11:54:48 -0600

    Playing with color scheme

--- functions.js
+++ functions.js
@@ -1,35 +1,35 @@
-function changeBackgroundColor()
-{
-	var colorNum = localStorage.getItem("bgcolor");
-	if(colorNum == 1)
-	{
-		localStorage.setItem("bgcolor", 0);
-		document.body.style.backgroundColor = "#113E3D";
-	}
-	else
-	{
-		localStorage.setItem("bgcolor", 1);
-		document.body.style.backgroundColor = "#4DC9C4";
-	}
-}
+// function changeBackgroundColor()
+// {
+// 	var colorNum = localStorage.getItem("bgcolor");
+// 	if(colorNum == 1)
+// 	{
+// 		localStorage.setItem("bgcolor", 0);
+// 		document.body.style.backgroundColor = "#113E3D";
+// 	}
+// 	else
+// 	{
+// 		localStorage.setItem("bgcolor", 1);
+// 		document.body.style.backgroundColor = "#4DC9C4";
+// 	}
+// }
 
-window.onload = function()
-{
-	var color = localStorage.getItem("bgcolor");
-	if(color === null)
-	{
-		localStorage.setItem("bgcolor", 1);
-		document.body.style.backgroundColor = "#4DC9C4";
-	}
-	else
-	{
-		if(color == 1)
-		{
-			document.body.style.backgroundColor = "#4DC9C4";
-		}
-		else
-		{
-			document.body.style.backgroundColor = "#113E3D";
-		}
-	}
-}
+// window.onload = function()
+// {
+// 	var color = localStorage.getItem("bgcolor");
+// 	if(color === null)
+// 	{
+// 		localStorage.setItem("bgcolor", 1);
+// 		document.body.style.backgroundColor = "#4DC9C4";
+// 	}
+// 	else
+// 	{
+// 		if(color == 1)
+// 		{
+// 			document.body.style.backgroundColor = "#4DC9C4";
+// 		}
+// 		else
+// 		{
+// 			document.body.style.backgroundColor = "#113E3D";
+// 		}
+// 	}
+// }
